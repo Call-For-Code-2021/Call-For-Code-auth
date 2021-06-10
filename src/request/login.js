6f9966b2ed75b9152b467089506b828ef7a9095d
kmbm0916 <kmbm0916.biz@gmail.com>
2021-06-10 15:18:32 +0900

    Update login.js

--- src/request/login.js
+++ src/request/login.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import axios from 'axios'
+import axios from 'axios';
 
 // async function user_get(){
 //     axios.get("https://charong.herokuapp.com/auth/login?id=asdffds")
